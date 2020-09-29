220a4a0cc1fea8fb81028ffbf94d040d5d2fa7ff
Nombuswandile <71407657+Nombuswandile@users.noreply.github.com>
2020-09-29 18:49:53 +0200

    made some changes to style

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
  
- //for them ham button
- function myFunction(x){
- 	test();
+ var pics = ["images/pink cake.jpg" ,"images/chocolate cake3.jpg", "images/yellow cake.jpg", "images/light blue cake.jpg"];
+	var index = 0;
+	//var panther = "black panther.jpg";
+	//var next = document.getElementById('next');
+	//var back = document.getElementById('back');
+	var image = document.getElementById('image');
+	image.src = pics[index];
+	image.style.height = '330px';
+
+
+//for them ham button
+function myFunction(x){
     x.classList.toggle("change");
-    test();
+    //var i = true;
+    var ham = document.getElementById('ham');
+	
+	//ham.addEventListener('click', function(){
+		var icons = document.getElementById('icons');
+		icons.style.marginTop = '40px';
+
+		ham.addEventListener('click', function(){
+			icons.style.marginTop = '-195px';
+			//remove eventlistener
+		})
+	//})
 }
 
-//for the contact icons on the header
-function test(){
-var ham = document.getElementById('ham');
-	var icons = document.getElementById('icons');
-	//var icon1 = document.getElementById('icon1');
-	//var icon2 = document.getElementById('icon2');
-	//var icon3 = document.getElementById('icon3');
-
-	var i = 1;
-
-	//if(i == 0){
-		//ham.addEventListener('click', function(){
-		    icons.style.marginTop = '40px'; //6px
-        
-		    ham.addEventListener('click', function(){
-			    icons.style.marginTop = '-195px';
-
-			    ham.addEventListener('click', function(){
-			    	 icons.style.marginTop = '40px'; //6px
-        
-		             ham.addEventListener('click', function(){
-			             icons.style.marginTop = '-195px';
-		             })
-			    })
-		   })
-		
-		    //i = i;
-	    //})
-    //}
-}
-
-
-
-//test();
+setInterval(function(){
+	index++;
+	if(index >= 0 && index != 3 ){
+		//index++;
+		image.style.height = '320px';  
+		image.src = pics[index];
+	}else if(index == 2){
+		image.style.height = '120px'; 
+		image.src = pics[index];
+		index = -1;
+	}else if(index == 3){
+		image.style.height = '320px';  
+		image.src = pics[index];
+		index = -1;
+	}else{
+		/*image.style.height = '270px'; 
+		image.src = pics[0];
+		index = ;*/
+		alert("index :" + index);
+	}
+}, 5000)
